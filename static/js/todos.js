a281cd6442f1841e0895613b9d0803a72082010b
jacques-cadet <mcadet347@gmail.com>
2018-02-19 09:23:38 -0500

    Add close table button

--- static/js/todos.js
+++ static/js/todos.js
@@ -1,4 +1,3 @@
-
 var service = "http://127.0.0.1:8000/router/entry/";
 var trHTML = '';
 var search = 0;
@@ -6,155 +5,163 @@ var search = 0;
 
 function getAppointments() {
 
-  // Show hidden table
-  $("#table").show();
+    // Show hidden table
+    $("#table").show();
+    $("#hideTableButton").show()
 
-  // If the table has not been populated show table 
-  // via search button, search "off"
-  if (!search) {
+    // If the table has not been populated show table 
+    // via search button, search "off"
+    if (!search) {
 
-    // "Get" the appointments from db
-    $.get(service, function(data) {
+        // "Get" the appointments from db
+        $.get(service, function(data) {
 
-      //Loop through database and create table rows and data
-      $.each(data, function(index) {
+            //Loop through database and create table rows and data
+            $.each(data, function(index) {
 
-        // Format date for table output
-        var mergdedDate = new Date(data[index].date +' '+ data[index].time);
+                // Format date for table output
+                var mergedDate = new Date(data[index].date + ' ' + data[index].time);
 
-        // Return formatted date
-        function formatDate() {
+                // Return formatted date
+                function formatDate() {
 
-          var monthNames = [
-            "Jan", "Feb", "Mar",
-            "Apr", "May", "Jun", "Jul",
-            "Aug", "Sep", "Oct",
-            "Nov", "Dec"
-          ];
+                    var monthNames = [
+                        "Jan", "Feb", "Mar",
+                        "Apr", "May", "Jun", "Jul",
+                        "Aug", "Sep", "Oct",
+                        "Nov", "Dec"
+                    ];
 
-          var dayNames = [
-            "Sun", "Mon", "Tue",
-            "Wed", "Thu", "Fri", "Sat",
-          ];
+                    var dayNames = [
+                        "Sun", "Mon", "Tue",
+                        "Wed", "Thu", "Fri", "Sat",
+                    ];
 
-          var day = mergdedDate.getDay();
-          var date = mergdedDate.getDate();
-          var month = mergdedDate.getMonth();
-          var year = mergdedDate.getFullYear();
+                    var day = mergedDate.getDay();
+                    var date = mergedDate.getDate();
+                    var month = mergedDate.getMonth();
+                    var year = mergedDate.getFullYear();
 
-          return dayNames[day] + ' ' +  monthNames[month] + ' ' + date + ', ' + year;
-        }
+                    return dayNames[day] + ' ' + monthNames[month] + ' ' +
+                        date + ', ' + year;
+                }
 
-        // Return formatted time
-        function formatTime() {
+                // Return formatted time
+                function formatTime() {
 
-          var min = mergdedDate.getMinutes();
-          var hour = mergdedDate.getHours();
-          var am_pm;
+                    var min = mergedDate.getMinutes();
+                    var hour = mergedDate.getHours();
+                    var am_pm;
 
-          //If the hour is passed 11 show pm else am
-          if (hour > 11) {
-            am_pm = "pm";
-          } else {
-            am_pm = "am";
-          }
+                    //If the hour is passed 11 show pm else am
+                    if (hour > 11) {
+                        am_pm = "pm";
+                    } else {
+                        am_pm = "am";
+                    }
 
-          // If the min is 9 or less prefix 0 to min, 09
-          if (min < 10){
-            min = '0'+ min;
-          }
+                    // If the min is 9 or less prefix 0 to min, 09
+                    if (min < 10) {
+                        min = '0' + min;
+                    }
 
-          // If the hour is 13 or more subtract 12. 17:30 to 5:30
-          if (hour >= 13) {
-            hour -= 12;
-          }
+                    // If the hour is 13 or more subtract 12. 17:30 to 5:30
+                    if (hour >= 13) {
+                        hour -= 12;
+                    }
 
-          // noon or midnight
-          if (min == 00 && hour == 12) {
-              return 'noon';
-          } else if (min == 00 && hour == 0) {
-              return 'midnight';
-          }
+                    // noon or midnight
+                    if (min == 00 && hour == 12) {
+                        return 'noon';
+                    } else if (min == 00 && hour == 0) {
+                        return 'midnight';
+                    }
 
-          if (hour == 0) {
-            hour = 12;
-          }
+                    if (hour == 0) {
+                        hour = 12;
+                    }
 
-          // If min == 00, don't show
-          if (min == 00) {
-            return  hour + ' ' + am_pm;
-          }
-          
-          return  hour + ':' + min + ' ' + am_pm;
-        }
+                    // If min == 00, don't show
+                    if (min == 00) {
+                        return hour + ' ' + am_pm;
+                    }
 
-        date = formatDate();
-        time = formatTime();
-        description = data[index].description;
+                    return hour + ':' + min + ' ' + am_pm;
+                }
 
-        trHTML += '<tr><td>' + date + '</td><td>' + time + 
-        '</td><td>' + description + '</td></tr>';
+                date = formatDate();
+                time = formatTime();
+                description = data[index].description;
 
-      });
+                trHTML += '<tr><td>' + date + '</td><td>' + time +
+                    '</td><td>' + description + '</td></tr>';
 
-      // Link to id'd hmtl tag
-      $('#location').append(trHTML);
-      
-    });
+            });
 
-    // If the table has been populated, turn search "on"
-    search = 1; 
-  } 
+            // Link to id'd hmtl tag
+            $('#location').append(trHTML);
 
-  if (search) {
+        });
 
-    $("#search").click(function() {
+        // If the table has been populated, turn search "on"
+        search = 1;
+    }
 
-      var input, filter, table, tableRow, tableData, index;
+    if (search) {
 
-      input = document.getElementById("books_input");
-      filter = input.value.toUpperCase(); // For case-sensitive search remove toUpperCase
-      table = document.getElementById("table");
-      tableRow = table.getElementsByTagName("tr");
+        $("#search").click(function() {
 
-      // Loop through all table rows, and hide those who don't match the search query
-      for (index = 0; index < tableRow.length; index++) {
+            var input, filter, table, tableRow, tableData, index;
 
-        tableData = tableRow[index].getElementsByTagName("td")[2];
+            input = document.getElementById("books_input");
+            filter = input.value.toUpperCase(); // For case-sensitive search remove toUpperCase
+            table = document.getElementById("table");
+            tableRow = table.getElementsByTagName("tr");
 
-        // If the tableRow has data
-        if (tableData) {
+            // Loop through all table rows, and hide those who don't match the search query
+            for (index = 0; index < tableRow.length; index++) {
 
-          // For case-sensitive search remove toUpperCase
-          if (tableData.innerHTML.toUpperCase().search(filter) > -1) {
-            tableRow[index].style.display = "";
-          } else {
-            tableRow[index].style.display = "none";
-          }
+                tableData = tableRow[index].getElementsByTagName("td")[2];
 
-        }
-      }
-    });
-  }
+                // If the tableRow has data
+                if (tableData) {
+
+                    // For case-sensitive search remove toUpperCase
+                    if (tableData.innerHTML.toUpperCase().search(filter) > -1) {
+                        tableRow[index].style.display = "";
+                    } else {
+                        tableRow[index].style.display = "none";
+                    }
+
+                }
+            }
+        });
+    }
 };
 
 
 $(document).ready(function() {
 
-  $('#search').click(getAppointments);
-  $("#table").hide();
-  $("#form").hide();
-  // $("#new").show();/
-
-  $("#new").click(function() {
-    $("#new").hide();
-    $("#form").show();
-    $("#add").show();
-  });
-
-  $("#cancel").click(function() {
-    $("#add").hide();
+    $('#search').click(getAppointments);
+    $("#hideTableButton").hide()
+    $("#table").hide();
     $("#form").hide();
-    $("#new").show();
-  });
+
+    $("#new").click(function() {
+        $("#new").hide();
+        $("#form").show();
+        $("#add").show();
+    });
+
+    $("#cancel").click(function() {
+        $("#add").hide();
+        $("#form").hide();
+        $("#new").show();
+    });
+
+    $("#hideTableButton").click(function() {
+        $("#table").hide()
+        $("#books_input").val("")
+        $("#hideTableButton").hide()
+    })
 });
